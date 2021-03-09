0671c7c14ec8d66d3af2447882c6ad3bcc982072
PinsaraK <pinsarakuruppu@gmail.com>
2021-03-09 00:14:48 -0500

    first commit

---
+++ src/views/LandingPage/index.js
@@ -0,0 +1,17 @@
+
+import React, { useState, Component, FunctionComponent, FC } from 'react';
+//import Modal from 'react-bootstrap/Modal';
+import constructionSign from '../../images/undraw_under_construction_46pa.svg'
+
+
+
+
+export const Landing = () => {
+
+  return (
+    <div>
+      <img src={constructionSign} className="h-48 mb-2" alt="team" />
+
+    </div>
+      )
+  }
